8051a50e2655464a6b4a890353d5bf218d5db5b5
aaronpdennis <aaron.p.dennis@gmail.com>
2016-05-26 14:11:11 -0400

    optional parameters

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 module.exports = function(options) {
   if (options.base.lastIndexOf('/') !== options.base.length - 1) options.base += '/';
+  var url = options.base + 'rest/services/'
+
+  if (options.service && !options.type) throw new Error('must specify service type');
+  if (!options.service && options.type) throw new Error('must specify service name');
+  if (options.service && options.type) url += options.service + '/' + options.type;
+
+  if (isInt(options.layerIndex)) url += '/' + options.layerIndex;
+
   if (!options.format) options.format = 'json';
-  return options.base + options.service + '/' + options.type + '/' + options.layerIndex + '?f=' + options.format;
+  url += '?f=' + options.format;
+
+  return url;
+}
+
+function isInt(value) {
+  var x = parseFloat(value);
+  return !isNaN(value) && (x | 0) === x;
 }
--- test/url.test.js
+++ test/url.test.js
@@ -2,7 +2,7 @@ var test = require('tape'),
     esriUrl = require('..');
 
 test('constructs url', function(t) {
-  t.plan(3);
+  t.plan(6);
 
   var url1 = esriUrl({
     base: 'https://services1.arcgis.com/test/arcgis/',
@@ -11,23 +11,43 @@ test('constructs url', function(t) {
     layerIndex: 0,
     format: 'pjson'
   });
-  t.equals(url1, 'https://services1.arcgis.com/test/arcgis/ServiceName/MapServer/0?f=pjson', 'returned expected url');
+  t.equals(url1, 'https://services1.arcgis.com/test/arcgis/rest/services/ServiceName/MapServer/0?f=pjson', 'returned expected url');
 
-  var url1 = esriUrl({
+  var url2 = esriUrl({
     base: 'https://services1.arcgis.com/test/arcgis/',
     service: 'Folder/AnotherFolder/ServiceName',
     type: 'FeatureServer',
     layerIndex: 4,
     format: 'json'
   });
-  t.equals(url1, 'https://services1.arcgis.com/test/arcgis/Folder/AnotherFolder/ServiceName/FeatureServer/4?f=json', 'returned expected url for within folders service');
+  t.equals(url2, 'https://services1.arcgis.com/test/arcgis/rest/services/Folder/AnotherFolder/ServiceName/FeatureServer/4?f=json', 'returned expected url for within folders service');
 
-  var url1 = esriUrl({
+  var url3 = esriUrl({
     base: 'https://services1.arcgis.com/gis/test/ArcGIS/',
     service: 'ServiceName',
     type: 'MapServer',
     layerIndex: 0
   });
-  t.equals(url1, 'https://services1.arcgis.com/gis/test/ArcGIS/ServiceName/MapServer/0?f=json', 'format defaults to json');
+  t.equals(url3, 'https://services1.arcgis.com/gis/test/ArcGIS/rest/services/ServiceName/MapServer/0?f=json', 'format defaults to json');
+
+  var url4 = esriUrl({
+    base: 'https://services1.arcgis.com/gis/test/ArcGIS',
+    service: 'ServiceName',
+    type: 'MapServer',
+    layerIndex: 0
+  });
+  t.equals(url4, 'https://services1.arcgis.com/gis/test/ArcGIS/rest/services/ServiceName/MapServer/0?f=json', 'last forward slash on base not required');
+
+  var url5 = esriUrl({
+    base: 'https://services1.arcgis.com/gis/test/ArcGIS/',
+    service: 'ServiceName',
+    type: 'MapServer',
+  });
+  t.equals(url5, 'https://services1.arcgis.com/gis/test/ArcGIS/rest/services/ServiceName/MapServer?f=json', 'layerIndex optional');
+
+  var url6 = esriUrl({
+    base: 'https://services1.arcgis.com/gis/test/ArcGIS/',
+  });
+  t.equals(url6, 'https://services1.arcgis.com/gis/test/ArcGIS/rest/services/?f=json', 'service and type optional');
 
 })
